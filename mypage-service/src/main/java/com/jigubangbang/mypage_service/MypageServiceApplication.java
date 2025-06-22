e78dec92979c3fcd2cc0bdbda4c6378485626bd1
Seunghyun Nam <shyunam@gmail.com>
2025-06-22 23:59:56 +0900

    feat: feign partial config

--- mypage-service/src/main/java/com/jigubangbang/mypage_service/MypageServiceApplication.java
+++ mypage-service/src/main/java/com/jigubangbang/mypage_service/MypageServiceApplication.java
@@ -3,9 +3,11 @@ package com.jigubangbang.mypage_service;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.jigubangbang.mypage_service.mapper")
+@EnableFeignClients
 public class MypageServiceApplication {
 
 	public static void main(String[] args) {
