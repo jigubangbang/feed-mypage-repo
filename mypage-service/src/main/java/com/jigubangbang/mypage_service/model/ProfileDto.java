ef3a8efeb3530f700a2f24e10205ba5bb06958c7
Seunghyun Nam <shyunam@gmail.com>
2025-06-24 11:22:50 +0900

    feat: follow/unfollow, travel status, date util function

--- mypage-service/src/main/java/com/jigubangbang/mypage_service/model/ProfileDto.java
+++ mypage-service/src/main/java/com/jigubangbang/mypage_service/model/ProfileDto.java
@@ -1,5 +1,7 @@
 package com.jigubangbang.mypage_service.model;
 
+import java.sql.Timestamp;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,17 +11,20 @@ public class ProfileDto {
     private String userId;
     private String nickname;
     private String profileImage;
-    private String createdAt;
+    private Timestamp createdAt;
     private String nationality;
-    private String nationalityLabel;
     private String bio;
-    private Character travelStyleId;
-    private String travelStyleName;
+    private String travelStyleId;
     private String travelStatus;
     private String mapColor;
     private int xp;
     private int level;
+
+    private String nationalityName;
+    private String travelStyleName;
     private int followerCount;
     private int followingCount;
     private int countryVisitCount;
+    private String formattedDate;
+    private boolean followStatus;
 }
--- mypage-service/src/main/java/com/jigubangbang/mypage_service/service/ProfileService.java
+++ mypage-service/src/main/java/com/jigubangbang/mypage_service/service/ProfileService.java
@@ -1,5 +1,10 @@
 package com.jigubangbang.mypage_service.service;
 
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +17,59 @@ public class ProfileService {
     private ProfileMapper profileMapper;
 
     public ProfileDto getProfileDto(String userId) {
-        return profileMapper.getProfile(userId);
+        ProfileDto profileDto = profileMapper.getProfile(userId);
+        
+        String countryId = profileDto.getNationality();
+        String travelStyleId = profileDto.getTravelStyleId();
+
+        if (countryId != null) {
+            profileDto.setNationalityName(profileMapper.getCountryName(countryId));
+        }
+
+        if (travelStyleId != null) {
+            profileDto.setTravelStyleName(profileMapper.getTravelStyleName(travelStyleId));
+        }
+
+        return profileDto;
+    }
+
+    public boolean updateTravelStatus(String userId, String travelStatus) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", userId);
+        map.put("travelStatus", travelStatus);
+
+        return profileMapper.updateTravelStatus(map) > 0;
+    }
+
+    public boolean getFollowStatus(String followerId, String followingId) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("followerId", followerId);
+        map.put("followingId", followingId);
+
+        return profileMapper.getFollowStatus(map); 
+    }
+
+    public List<String> getFollowers(String userId) {
+        return profileMapper.getFollowers(userId);
+    }
+    
+    public List<String> getFollowing(String userId) {
+        return profileMapper.getFollowing(userId);
+    }
+
+    public boolean followUser(String followerId, String followingId) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("followerId", followerId);
+        map.put("followingId", followingId);
+
+        return profileMapper.followUser(map) > 0;
+    }
+
+    public boolean unfollowUser(String followerId, String followingId) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("followerId", followerId);
+        map.put("followingId", followingId);
+
+        return profileMapper.unfollowUser(map) > 0;
     }
 }
