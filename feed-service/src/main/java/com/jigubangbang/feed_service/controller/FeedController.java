5e496bb4fbb72f56f6631562f7e871fe048a12c9
Seunghyun Nam <shyunam@gmail.com>
2025-07-15 17:11:14 +0900

    fix: feed date filtering (inclusive overlap)

--- feed-service/src/main/java/com/jigubangbang/feed_service/controller/FeedController.java
+++ feed-service/src/main/java/com/jigubangbang/feed_service/controller/FeedController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.jigubangbang.feed_service.chat_service.NotificationServiceClient;
-import com.jigubangbang.feed_service.mapper.CountryMapper;
 import com.jigubangbang.feed_service.model.CommentDto;
 import com.jigubangbang.feed_service.model.FeedDto;
 import com.jigubangbang.feed_service.model.FeedImageDto;
